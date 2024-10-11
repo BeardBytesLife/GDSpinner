3631d64f9b97c073dc44d2fdef819c6f0e430735
TrentSaggus <trent@saggus.me>
2024-10-11 17:44:30 +1100

    Created Generic Debug Array of items. Linked Item Delete Button to global Signal and implimented item deletion from Array. Implimented 'spin' button which selects random item from array and prints to screen. Added code comments to enable others to assist in development.

--- Scripts/array_list_item.gd
+++ Scripts/array_list_item.gd
@@ -1,8 +1,14 @@
 class_name ArrayListItem extends HBoxContainer
 
 @export var label: Label
+#Emitting the "delete" button signal to the rest of the program.
+signal deleteListItem(ArrayListItem)
 
 var text: String:
 	set(value):
 		text = value
 		label.text = value
+
+#This takes the signal sent by the button, and sends it to the signal "deleteListItem" for retransmition to the rest of the program.
+func DeleteButtonPressed():
+	deleteListItem.emit(self)
