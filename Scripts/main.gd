adae780728550b06231538cb9ef426c9d866cbef
TrentSaggus <trent@saggus.me>
2024-10-12 20:14:47 +1100

    Corrected 'SpinButtonPressed' to check if List is empty and if empty pass else execute function

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -5,10 +5,10 @@ var List: Array[ArrayListItem]
 var listItemScene = preload("res://Scenes/ArrayListItem.tscn")
 
 #For Debugging Purpouses, we are creating a default list of items as soon as the program loads. Comment out from final build, save for future debugging purpouses.
-func _ready():
-	var values: Array[String] = ["Item 1","Item 2","Item 3","Item 4","Item 5","Item 6","Item 7","Item 8","Item 9","Item 10"]
-	for i in values:
-		appendList(i)
+#func _ready():
+	#var values: Array[String] = ["Item 1","Item 2","Item 3","Item 4","Item 5","Item 6","Item 7","Item 8","Item 9","Item 10"]
+	#for i in values:
+		#appendList(i)
 
 
 func appendList(text=""):
@@ -34,5 +34,9 @@ func deleteItem(listItem: ArrayListItem):
 func _on_spin_button_pressed():
 	#When the SpinButton is pressed, we assign a random item from the array to the chosenItem String and then assign that to the Label which displays the random item.
 	#This function will need to change and evolve to accomodate the spinning animation once implimented.
-	var chosenItem: String = List.pick_random().text
-	$SpinContainer/RandomItem.text = chosenItem
+	#Added if statement to check if list of items is empty, and if empty do nothing otherwise app crashes.
+	if List.is_empty():
+		pass
+	else:
+		var chosenItem: String = List.pick_random().text
+		$SpinContainer/RandomItem.text = chosenItem
